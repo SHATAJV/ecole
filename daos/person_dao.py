a6d1d6fb5e56baf923f9d7fab5f56ba36c987880
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-08-27 13:15:26 +0200

    add modification

--- daos/person_dao.py
+++ daos/person_dao.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Classe Dao[Course]
+Classe Dao[Person]
 """
 
 from models.person import Person
@@ -11,51 +11,50 @@ from typing import Optional
 
 
 @dataclass
-class CourseDao(Dao[Person]):
+class PersonDao(Dao[Person]):
     def create(self, person: Person) -> int:
-        """Crée en BD l'entité Course correspondant au cours obj
+        """Crée en BD l'entité Person correspondant à l'objet person
 
-        :param course: à créer sous forme d'entité Course en BD
+        :param person: Personne à créer sous forme d'entité Person en BD
         :return: l'id de l'entité insérée en BD (0 si la création a échoué)
         """
         with Dao.connection.cursor() as cursor:
-            sql = "INSERT INTO course (first_name, last_name, age) VALUES (%s, %s, %s)"
+            sql = "INSERT INTO person (first_name, last_name, age) VALUES (%s, %s, %s)"
             cursor.execute(sql, (person.first_name, person.last_name, person.age))
             Dao.connection.commit()
             return cursor.lastrowid
 
-
-    def read(self, id_course: int) -> Optional[Person]:
-        """Renvoit le cours correspondant à l'entité dont l'id est id_course
+    def read(self, id_person: int) -> Optional[Person]:
+        """Renvoit la personne correspondant à l'entité dont l'id est id_person
            (ou None s'il n'a pu être trouvé)"""
         with Dao.connection.cursor() as cursor:
-            sql = "SELECT * FROM course WHERE id_person=%s"
-            cursor.execute(sql, (id_person))
+            sql = "SELECT * FROM person WHERE id_person=%s"
+            cursor.execute(sql, (id_person,))
             result = cursor.fetchone()
-        return result
+            if result:
+                return Person(*result)
+            return None
 
-    def update(self, course: Person) -> bool:
-        """Met à jour en BD l'entité Course correspondant à course, pour y correspondre
+    def update(self, person: Person) -> bool:
+        """Met à jour en BD l'entité Person correspondant à person
 
-        :param course: cours déjà mis à jour en mémoire
+        :param person: personne déjà mise à jour en mémoire
         :return: True si la mise à jour a pu être réalisée
         """
         with Dao.connection.cursor() as cursor:
-            sql = "UPDATE course SET first_name=%s, last_name=%s, age=%s WHERE id_person=%s"
-
-            cursor.execute(sql, (course.first_name, course.last_name, course.age))
+            sql = "UPDATE person SET first_name=%s, last_name=%s, age=%s WHERE id_person=%s"
+            cursor.execute(sql, (person.first_name, person.last_name, person.age))
             Dao.connection.commit()
             return cursor.rowcount > 0
 
     def delete(self, person: Person) -> bool:
-        """Supprime en BD l'entité Course correspondant à course
+        """Supprime en BD l'entité Person correspondant à person
 
-        :param course: cours dont l'entité Course correspondante est à supprimer
+        :param person: personne dont l'entité Person correspondante est à supprimer
         :return: True si la suppression a pu être réalisée
         """
         with Dao.connection.cursor() as cursor:
-            sql = "DELETE FROM course WHERE id_course=%s"
-            cursor.execute(sql, (person.id_person,))
+            sql = "DELETE FROM person WHERE id_person=%s"
+            cursor.execute(sql, (person.id_person))
             Dao.connection.commit()
             return cursor.rowcount > 0
-
